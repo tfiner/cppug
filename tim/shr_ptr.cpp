1bb91201756a239e6d1de373726b8c2c1eeff1f9
Tim Finer <tfiner@fastmail.fm>
2011-11-25 08:12:32 -0800

    Added a sample for Eric.

---
+++ tim/shr_ptr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+#include <list>
+#include <algorithm>
+
+class A {
+public:
+    virtual void update() = 0;
+};
+
+class B : public A {
+public:
+    virtual void update() { std::cout << __FUNCTION__ << std::endl; }
+};
+
+typedef std::shared_ptr< A > APtr;
+typedef std::list< APtr > APtrs;
+
+int main() {
+    APtrs aptrs;
+    aptrs.push_back( APtr(new B) );
+    for ( auto ap : aptrs )
+        ap->update();
+
+    for ( APtrs::iterator i = aptrs.begin(); i != aptrs.end(); ++i )
+        (*i)->update();
+
+    for ( auto i = aptrs.begin(); i != aptrs.end(); ++i )
+        (*i)->update();
+
+    std::for_each( aptrs.begin(), aptrs.end(), [](APtr a) { a->update(); } );
+
+    return 0;
+}
