15fa98c83c488370ef7b931d3af35eb1e3c25649
Tim Finer <tfiner@fastmail.fm>
2011-11-25 08:38:35 -0800

    Added some comments on the loops, reordered in order of compiler version.

--- tim/shr_ptr.cpp
+++ tim/shr_ptr.cpp
@@ -23,15 +23,20 @@ typedef std::list< APtr > APtrs;
 int main() {
     APtrs aptrs;
     aptrs.push_back( APtr(new B) );
-    for ( auto ap : aptrs )
-        ap->update();
 
+    // C++98, this works for just about all compilers.
     for ( APtrs::iterator i = aptrs.begin(); i != aptrs.end(); ++i )
         (*i)->update();
 
+    // C++11 auto keyword, compiler knows the type automatically.
     for ( auto i = aptrs.begin(); i != aptrs.end(); ++i )
         (*i)->update();
 
+    // C++11, love the new for each.  Only GCC46
+    for ( auto ap : aptrs )
+        ap->update();
+
+    // C++11 lambda, lambda, bo-blamda!  GCC46 & VS 2010 SP1 can do this.
     std::for_each( aptrs.begin(), aptrs.end(), [](APtr a) { a->update(); } );
 
     return 0;
